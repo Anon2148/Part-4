90841ebb46a496028185241964fd4c715faff05e
Anon2148 <privext@tutanota.com>
2024-08-03 17:36:09 +0200

    feat: exercise 4.2, Blog List step 2 done

---
+++ controllers/blogs.js
@@ -0,0 +1,22 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  if (!blog.title || !blog.author || !blog.url || !blog.likes) {
+    return response.status(400).json({ error: 'malformed request' })
+  }
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+module.exports = blogsRouter
