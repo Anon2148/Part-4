07ae4e89f8d85b673a4942b8ed84d05714205b26
Anon2148 <privext@tutanota.com>
2024-09-14 16:34:57 +0200

    feat: exercise 4.22(*), Blog List Expansion step 10 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,17 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const jwt = require('jsonwebtoken')
-const User = require('../models/user')
+const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
   const body = request.body
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
-  if (!body.title || !body.url) {
+  if (!body.title || !body.url || !user) {
     return response.status(400).json({ error: 'malformed request' })
   } else if (!body.likes) {
     body.likes = 0
@@ -37,24 +32,24 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
+blogsRouter.delete(
+  '/:id',
+  middleware.userExtractor,
+  async (request, response) => {
+    const blog = await Blog.findById(request.params.id)
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
+    const user = request.user
 
-  if (blog.user.toString() === user.id.toString()) {
-    await blog.deleteOne()
-    return response.status(204).end()
-  } else {
-    return response.status(401).json({
-      error: 'you are trying to delete a blog that does not belong to you',
-    })
+    if (blog.user.toString() === user.id.toString()) {
+      await blog.deleteOne()
+      return response.status(204).end()
+    } else {
+      return response.status(401).json({
+        error: 'you are trying to delete a blog that does not belong to you',
+      })
+    }
   }
-})
+)
 
 blogsRouter.put('/:id', async (request, response) => {
   const body = request.body
